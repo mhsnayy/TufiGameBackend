12aaf1774b50c8a9e9f3da6026539ace8ce4a4ad
mhsnayy <muhsinay06@gmail.com>
2025-10-21 09:42:43 +0300

    Add project files.

---
+++ TufiGame.Entity/Concrete/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TufiGame.Entity.Abstract;
+
+namespace TufiGame.Entity.Concrete
+{
+    public class Item:IEntity
+    {
+        public int Id { get; set; }
+        public int GameId { get; set; }
+        public int CustomerId { get; set; }
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+
+    }
+}
